5611fa8e906800c8b6347aad5aefb56db3f6b4a1
Stephan Maevers <stephan.maevers@gmail.com>
2026-08-27 21:27:23 +0200

    refactor: remove obsolete toolbar export

--- lib/editor/editor_toolbar.dart
+++
@@ -1,9 +0,0 @@
-import '../ui/features/editor/views/editor_toolbar_view.dart';
-
-/// Transitional export for callers that still import the pre-feature toolbar
-/// path. Production editor code uses [EditorToolbarView] directly.
-export '../ui/features/editor/views/editor_toolbar_view.dart'
-    show EditorToolbarView;
-
-/// Compatibility name retained while downstream tests and embedders migrate.
-typedef EditorToolbar = EditorToolbarView;
